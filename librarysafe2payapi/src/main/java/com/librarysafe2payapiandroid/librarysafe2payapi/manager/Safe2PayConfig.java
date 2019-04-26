9368b786e99f392d940365b024f675a1db34830c
luisgouvea <luiseduardo.gouvea@gmail.com>
2019-04-26 17:59:52 -0300

    vfdvfd gdf

---
+++ librarysafe2payapi/src/main/java/com/librarysafe2payapiandroid/librarysafe2payapi/manager/Safe2PayConfig.java
@@ -0,0 +1,9 @@
+package com.librarysafe2payapiandroid.librarysafe2payapi.manager;
+
+public class Safe2PayConfig {
+    public static String Key = "";
+
+    public static void setKey(String key) {
+        Safe2PayConfig.Key = key;
+    }
+}
--- librarysafe2payapi/src/main/java/com/librarysafe2payapiandroid/librarysafe2payapi/manager/singleSale/SingleSaleIntegration.java
+++ librarysafe2payapi/src/main/java/com/librarysafe2payapiandroid/librarysafe2payapi/manager/singleSale/SingleSaleIntegration.java
@@ -1,15 +1,14 @@
-package com.librarysafe2payapiandroid.librarysafe2payapi.manager;
+package com.librarysafe2payapiandroid.librarysafe2payapi.manager.singleSale;
 
 import com.librarysafe2payapiandroid.librarysafe2payapi.services.singleSale.add.SingleSaleAddCallback;
 import com.librarysafe2payapiandroid.librarysafe2payapi.services.singleSale.add.SingleSaleAddRequest;
 
-public class RequestManager {
+public class SingleSaleIntegration {
 
     private SingleSaleAddRequest singleSaleAddRequest;
 
-    public RequestManager() {
+    public SingleSaleIntegration() {
     }
-
 
     public void addSingleSale(SingleSaleAddCallback callback, double amount, int status) {
         this.singleSaleAddRequest = new SingleSaleAddRequest(callback);
