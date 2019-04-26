123c0b0e53d623b521743a58e57bc06be9b1e918
luisgouvea <luiseduardo.gouvea@gmail.com>
2019-04-26 14:52:57 -0300

    initial commit library

---
+++ librarysafe2payapi/src/main/java/com/librarysafe2payapiandroid/librarysafe2payapi/manager/RequestManager.java
@@ -0,0 +1,15 @@
+package com.librarysafe2payapiandroid.librarysafe2payapi.manager;
+
+public class RequestManager {
+
+//    private SingleSaleAddRequest singleSaleAddRequest;
+//
+//    public RequestManager() {
+//    }
+//
+//
+//    public void addSingleSale(SingleSaleAddCallback callback, double amount, int status) {
+//        this.singleSaleAddRequest = new SingleSaleAddRequest(callback);
+//        singleSaleAddRequest.addSingleSale(amount, status);
+//    }
+}
