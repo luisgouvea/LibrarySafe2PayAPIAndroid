53c1d0be60cff6267bd530f844fff0ca7212c3e7
luisgouvea <luiseduardo.gouvea@gmail.com>
2019-04-26 18:06:57 -0300

    fes few

--- librarysafe2payapi/src/main/java/com/librarysafe2payapiandroid/librarysafe2payapi/manager/Safe2PayConfig.java
+++ librarysafe2payapi/src/main/java/com/librarysafe2payapiandroid/librarysafe2payapi/manager/Safe2PayConfig.java
@@ -1,7 +1,7 @@
 package com.librarysafe2payapiandroid.librarysafe2payapi.manager;
 
 public class Safe2PayConfig {
-    public static String Key = "";
+    private static String Key = "";
 
     public static void setKey(String key) {
         Safe2PayConfig.Key = key;
